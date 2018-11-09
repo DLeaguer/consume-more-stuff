08e1767ac3c75dd344ac83c4ffce035f9092e0ad
Jason <Jason@Colins-MBP.home>
2018-11-08 17:12:41 -1000

    add mock data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = 
+    {
+      bobbles: [
+        {
+          id: 1,
+          title: "President Obama",
+          description: "Prez Obama bobble head",
+          price: 15,
+          image: "https://papundits.files.wordpress.com/2011/08/obama-bobblehead.gif",
+          condition: "Excellent",
+          category: "Presidents",
+          status: true
+        },
+        {
+          id: 2,
+          title: "Michael Jackson",
+          description: "Michael Jackson bobble head",
+          price: 10,
+          image: "http://blog.swagbucks.com/wp-content/uploads/2012/02/71o-800ojqL._AA1500_.jpg",
+          condition: "Good",
+          category: "Singers",
+          status: true
+        },
+        {
+          id: 3,
+          title: "Angelina Jolie",
+          description: "Maleficent bobble head",
+          price: 12,
+          image: "https://experiencethemistress.com/wp-content/uploads/2016/06/2014-Funko-Maleficent-Movie-Wacky-Wobbler-Bobble-Head-Vinyl-Figure-Unreleased-Prototype-002.jpg",
+          condition: "Poor",
+          category: "Actors",
+          status: true
+        },
+      ]
+    };
+  }
   render() {
     return (
       <div className="App">
