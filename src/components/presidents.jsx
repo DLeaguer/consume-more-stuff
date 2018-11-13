ae6fff506ce919c7c1c29fa6efe25d9f6da6844e
DLeaguer <immostlikely@hotmail.com>
2018-11-12 17:00:18 -1000

    show all bobbles in presidents.jsx

--- src/components/presidents.jsx
+++ src/components/presidents.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Presidents(props) {
   console.log('Presidents props', props)
   return (
-    <div>Presidents component</div>
+    // <div>Presidents component</div>
+    props.bobbles.map(item => <div
+      className='bobbleCards'><a href={item.image} target='blank'><img src={item.image} alt="bobblehead.png" /></a> <br/> {item.title} <br/> {item.description} <br/> {item.price} <br/>
+      <button className='deleteButton' onClick={ () => {props.deleteBobble(item.id)}}>Delete</button>
+    </div>)
   )
 }
 
