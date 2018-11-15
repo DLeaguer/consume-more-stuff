3029421e3b77c3a443b85e19acf9e45c143296ba
Jason <Jason@Colins-MBP.home>
2018-11-14 21:16:55 -1000

    change actor info on view-all

--- src/components/view-all.jsx
+++ src/components/view-all.jsx
@@ -5,7 +5,7 @@ function ViewAll(props) {
   console.log('view all props', props)
   return (
     props.bobbles.map(item => <div
-      className='bobbleCards'><a href={item.image} target='blank'><img className='catImg' src={item.image} alt="bobblehead.png" /></a> <br/> {item.title} <br/> {item.description} <br/> ${item.price}.00 <br/>
+      className='bobbleCards'><a href={item.image} target='blank'><img className='catImg' src={item.image} alt="bobblehead.png" /></a> <br/> {item.title} <br/> {item.description} <br/> {item.category} <br/> ${item.price}.00 <br/>
       <button className='deleteButton' onClick={ () => {props.deleteBobble(item.id)}}>Delete</button> <hr/>
       <p id='editFormTitle'>Edit Form</p>
       <EditForm editBobble={props.editBobble} bobbles={props.bobbles} id={item.id} />
