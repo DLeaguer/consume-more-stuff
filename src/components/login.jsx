ace90290ba077c6c2ed75b444f3b2d3575aac314
Jason <Jason@Colins-MBP.home>
2018-11-16 16:56:20 -1000

    connect login and register user routes

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { isNull } from 'util';
+// import axios from "axios";
 
-function Login(props) {
-  console.log('login props', props)
-  return (
-    <div>Login component</div>
-  )
-}
+class Login extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        email: null,
+        password: null
+      }
+      console.log('Register props', props)
+    }
 
-export default Login;
+    
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('submitted!', this.state)
+        this.props.loginUser(this.state)
+    }
+    
+    handleChange = (e) => {
+        e.preventDefault()
+        const { name, value } = e.target
+        this.setState({
+            [name] : value
+        })
+    }
+    
+    render() {
+        return (
+          <div className="loginPage">
+            <h1>
+              Login Page
+            </h1>
+            <div className="loginContainer">
+              <form onSubmit={this.handleSubmit}>
+                <br/>
+                  <label> Email: <br/> <input onChange={this.handleChange} type='text' name='email' placeholder='email'></input>
+                  </label>
+                  <br/>
+                  <label> Password: <br/> <input onChange={this.handleChange} type='text' name='password' placeholder='password'></input>
+                  </label> <br/>
+                  <button type='submit'>Login</button>
+              </form>
+            </div>
+          </div>
+      )
+    }
+  }
+  
+  export default Login;
