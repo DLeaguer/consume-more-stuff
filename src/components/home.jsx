d99789be81fa1fdc1dbda173d399813fabf13e81
CwLauger <cwlauger@gmail.com>
2018-11-20 18:28:50 -1000

    cssScheme works

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home(props) {
   console.log('Home props', props)
   return (
-    <div>Home Component
+    <div>
       <div>
         <a href="https://giphy.com/gifs/puppy-black-and-white-2FhASosZtLUPe" target='blank'><img  className='homeImg' src={require('../BobbleBay.png')} alt="BobbleBay" /></a>
       </div>
